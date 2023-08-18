c8255c7787bf9b15e1f7b1b4c86825c25fd1f51f
akutuh <akutuh@uef.fi>
2023-08-18 16:55:25 +0300

    1.7 unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -17,25 +17,57 @@ const Statistics = ({ text, value }) => {
   )
 }
 
+const Average = ({ good, bad, total}) => {
+  if (total === 0) {
+    return (
+      <p>average 0</p>
+    )
+  }
+  return (
+    <>
+      <p>average {(good - bad)/total}</p>
+    </>
+  )
+}
+
+const Positive = ({ good, total }) => {
+  if (total === 0) {
+    return (
+      <>
+        <p>positive 0</p>
+      </>
+    )
+  }
+  return (
+    <>
+      <p>positive {(100 * good) / total}%</p>
+    </>
+  )
+}
+
 const App = () => {
   //tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
+    setTotal(total + 1)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setTotal(total + 1)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setTotal(total + 1)
   }
 
   return (
@@ -48,6 +80,9 @@ const App = () => {
       <Statistics value={good} text='good' />
       <Statistics value={neutral} text='neutral'/>
       <Statistics value={bad} text='bad'/>
+      <p>all {total}</p>
+      <Average good={good} bad={bad} total={total}/>
+      <Positive good={good} total={total}/>
     </div>
   )
 }
