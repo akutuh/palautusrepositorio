a4143a5bcefbc8ba5b6046d89482b556157b4317
akutuh <akutuh@uef.fi>
2023-08-23 15:08:13 +0300

    os1 kurssitiedot pohja

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+/* eslint-disable react/prop-types */
+const Header = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>{props.course.name}</h1>
+    </>
+  );
+};
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default App;
