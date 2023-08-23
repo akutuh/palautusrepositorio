7ce2feffa6e70d602433d9f290f81925981bd827
akutuh <akutuh@uef.fi>
2023-08-23 12:30:33 +0300

    1.8 unicafe step 3

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,34 +1,47 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
+import { useState } from "react";
 
-const Button = ({ handleClick, text}) => {
+const Statistics = ({ good, neutral, bad, total }) => {
+  return (
+    <div>
+      <Rating value={good} text="good" />
+      <Rating value={neutral} text="neutral" />
+      <Rating value={bad} text="bad" />
+      <p>all {total}</p>
+      <Average good={good} bad={bad} total={total} />
+      <Positive good={good} total={total} />
+    </div>
+  );
+};
+
+const Button = ({ handleClick, text }) => {
   return (
     <>
       <button onClick={handleClick}>{text}</button>
     </>
-  )
-}
+  );
+};
 
-const Statistics = ({ text, value }) => {
+const Rating = ({ text, value }) => {
   return (
     <>
-      <p>{text} {value}</p>
+      <p>
+        {text} {value}
+      </p>
     </>
-  )
-}
+  );
+};
 
-const Average = ({ good, bad, total}) => {
+const Average = ({ good, bad, total }) => {
   if (total === 0) {
-    return (
-      <p>average 0</p>
-    )
+    return <p>average 0</p>;
   }
   return (
     <>
-      <p>average {(good - bad)/total}</p>
+      <p>average {(good - bad) / total}</p>
     </>
-  )
-}
+  );
+};
 
 const Positive = ({ good, total }) => {
   if (total === 0) {
@@ -36,55 +49,50 @@ const Positive = ({ good, total }) => {
       <>
         <p>positive 0</p>
       </>
-    )
+    );
   }
   return (
     <>
       <p>positive {(100 * good) / total}%</p>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   //tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const handleGoodClick = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    setTotal(total + 1)
-  }
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(total + 1);
+  };
 
   const handleNeutralClick = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    setTotal(total + 1)
-  }
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(total + 1);
+  };
 
   const handleBadClick = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    setTotal(total + 1)
-  }
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(total + 1);
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text='good' />
-      <Button handleClick={handleNeutralClick} text='neutral' />
-      <Button handleClick={handleBadClick} text='bad' />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
-      <Statistics value={good} text='good' />
-      <Statistics value={neutral} text='neutral'/>
-      <Statistics value={bad} text='bad'/>
-      <p>all {total}</p>
-      <Average good={good} bad={bad} total={total}/>
-      <Positive good={good} total={total}/>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
