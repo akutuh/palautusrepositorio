896e5d0d04c019e2b5c5399763de64571639acac
akutuh <akutuh@uef.fi>
2023-08-23 15:25:28 +0300

    2.1 kurssitiedot step6

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,26 +1,26 @@
 /* eslint-disable react/prop-types */
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
     <>
-      <h1>{props.course.name}</h1>
+      <h1>{course.name}</h1>
     </>
   );
 };
-const Content = (props) => {
+const Content = ({ course }) => {
   return (
-    <>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </>
+    <div>
+      {course.parts.map((crse) => (
+        <Part course={crse} key={crse.id} />
+      ))}
+    </div>
   );
 };
 
-const Part = (props) => {
+const Part = ({ course }) => {
   return (
     <>
       <p>
-        {props.part.name} {props.part.exercises}
+        {course.name} {course.exercises}
       </p>
     </>
   );
@@ -39,30 +39,41 @@ const Total = (props) => {
   );
 };
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
     name: "Half Stack application development",
+    id: 1,
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   );
 };
