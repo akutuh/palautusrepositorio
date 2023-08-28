3b32fdf1bba8ab92d234127e26cc24de780079fe
akutuh <akutuh@uef.fi>
2023-08-28 19:31:24 +0300

    2.13 puhelinluettelo step8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from "react"
 import PersonForm from "./components/PersonForm"
 import Filter from "./components/Filter"
 import Persons from "./components/Persons"
-import axios from "axios"
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,10 +11,10 @@ const App = () => {
   const [filter, setFilter] = useState("")
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -29,10 +29,10 @@ const App = () => {
       setNewName("")
       setNewNumber("")
     } else {
-      axios
-        .post('http://localhost:3001/persons', nameObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personService
+        .create(nameObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
         })
